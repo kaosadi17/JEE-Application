449490f1c276bf7710f152766af69567bf805108
amalio <amali.osadi@trycake.com>
2017-08-21 09:43:41 +0530

    configure web inf file

--- MyFirstProject/src/com/amali/surverlet/HelloWorld.java
+++ MyFirstProject/src/com/amali/surverlet/HelloWorld.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/HelloWorld")
+@WebServlet("/hidden/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
